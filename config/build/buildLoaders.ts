4da3ab9e51def6c69a179b75dc8888c591579b4d
Maria Osokina <osokina.m@medindex.ru>
2023-02-22 17:21:17 +0300

    Lesson-2.17 Babel. Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -28,6 +28,28 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     ],
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', { targets: "defaults" }]
+        ],
+        "plugins": [
+          [
+              "i18next-extract",
+              {
+                  locales: ['ru', 'en'],
+                  keyAsDefaultValue: true
+              }
+          ],
+      ]
+      }
+    }
+  }
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -46,7 +68,8 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   return [
     fileLoader,
     svgLoader,
-      typescriptLoader,
-      cssLoader
-      ]
+    babelLoader,
+    typescriptLoader,
+    cssLoader
+  ]
 }
