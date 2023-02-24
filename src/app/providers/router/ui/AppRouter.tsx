895052b5c27325fbe02cb769ac01ec183f41b3b5
Maria Osokina <osokina.m@medindex.ru>
2023-02-24 16:24:12 +0300

    Lesson-2.21 NotFoundPage & PageLoader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,20 +1,25 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { RouteConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(RouteConfig).map(({ element, path }) => (
-                    <Route
-                        key={path}
-                        element={<div className='page-wrapper'>{element}</div>}
-                        path={path}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+
+        <Routes>
+            {Object.values(RouteConfig).map(({ element, path }) => (
+                <Route
+                    key={path}
+                    element={
+                        <Suspense fallback={<PageLoader />}>
+                            <div className='page-wrapper'>{element}</div>
+                        </Suspense>
+                    }
+                    path={path}
+                />
+            ))}
+        </Routes>
+
     );
 };
 
